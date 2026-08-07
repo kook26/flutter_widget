fa8e2d2c33a0e16bd61e69d58df716fa2b26e3a4
kasahara <>
2026-08-07 14:33:09 +0900

    fittedBoxの学習

--- lib/main.directories.g.dart
+++ lib/main.directories.g.dart
@@ -30,6 +30,8 @@ import 'package:flutter_widget/widgets/drawer.dart'
     as _flutter_widget_widgets_drawer;
 import 'package:flutter_widget/widgets/Expanded.dart'
     as _flutter_widget_widgets_Expanded;
+import 'package:flutter_widget/widgets/fitted_box.dart'
+    as _flutter_widget_widgets_fitted_box;
 import 'package:flutter_widget/widgets/flexible.dart'
     as _flutter_widget_widgets_flexible;
 import 'package:flutter_widget/widgets/icon.dart'
@@ -146,6 +148,15 @@ final directories = <_widgetbook.WidgetbookNode>[
           _widgetbook.WidgetbookUseCase(
             name: 'Expanded',
             builder: _flutter_widget_widgets_Expanded.expandedSample,
+          ),
+        ],
+      ),
+      _widgetbook.WidgetbookComponent(
+        name: 'FittedBoxSample',
+        useCases: [
+          _widgetbook.WidgetbookUseCase(
+            name: 'FittedBox',
+            builder: _flutter_widget_widgets_fitted_box.fittedBoxSample,
           ),
         ],
       ),
