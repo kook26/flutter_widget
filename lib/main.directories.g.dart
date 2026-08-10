7fbe846bab0601ceadbf787c64d2121518383e47
kasahara <>
2026-08-10 15:50:20 +0900

    AnimatedSwitcherの学習

--- lib/main.directories.g.dart
+++ lib/main.directories.g.dart
@@ -12,6 +12,8 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:flutter_widget/widgets/align.dart'
     as _flutter_widget_widgets_align;
+import 'package:flutter_widget/widgets/animated_switcher.dart'
+    as _flutter_widget_widgets_animated_switcher;
 import 'package:flutter_widget/widgets/app_bar.dart'
     as _flutter_widget_widgets_app_bar;
 import 'package:flutter_widget/widgets/aspect_ratio.dart'
@@ -133,6 +135,16 @@ final directories = <_widgetbook.WidgetbookNode>[
           _widgetbook.WidgetbookUseCase(
             name: 'Align',
             builder: _flutter_widget_widgets_align.alignSample,
+          ),
+        ],
+      ),
+      _widgetbook.WidgetbookComponent(
+        name: 'AnimatedSwitcherSample',
+        useCases: [
+          _widgetbook.WidgetbookUseCase(
+            name: 'AnimatedSwitcher',
+            builder: _flutter_widget_widgets_animated_switcher
+                .animatedSwitcherSample,
           ),
         ],
       ),
